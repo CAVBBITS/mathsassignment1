bc358aaa0bba6f6359c86dc898655533b24c4874
cavbtech <cavbtech@gmail.com>
2021-12-06 19:23:31 +0530

    Precision added

--- question-2/gauss_elimination2.py
+++ question-2/gauss_elimination2.py
@@ -1,6 +1,6 @@
-# This modules consists functions to implement Gauss elimination with and without pivoting.
+# This modules consists functions to implement guass elimination with and without pivoting.
 # It also counts the number of additions, multiplications
-# and divisions performed during Gaussian elimination.
+# and divisions performed during guassian elimination.
 import numpy as np
 
 class ERROR_IN_PARTITIAL_PIVOT_STEPS(Exception):
@@ -19,110 +19,120 @@ class ERROR_IN_BACK_SUBSTITUTION_STEPS(Exception):
         self.message = message
         super().__init__(self.message)
 
-def partial_pivot(a_matrix:np.array, head_row_index:int)->np.array:
+class GaussElimination:
     """
-    partial_pivot methods applies row pivot on a_matrix by comparing curr_row_index with subsequent rows
-    and swaps the rows if the a_matrix[curr_row_index,column_index] with a_matrix[max_row_index,column_index]
-    :param a_matrix:
-    :param head_row_index:
-    :return:
+        GaussElimination is the class which initializes parameters.
+        The constructor expects precision.  Default precision is 5
     """
-    max_value_row_index = head_row_index
-    ## This is same as row_index because, the guass elimination works with nxn matrix
-    head_col_index     = head_row_index
-    print(f"head_row_index={head_row_index} and length = {len(a_matrix)} range = {range(head_row_index + 1, 1, len(a_matrix)+1)}")
-    for next_row_index in range(head_row_index + 1, len(a_matrix),1):
-        print(f" printing next_row_index ={next_row_index} ")
-        if a_matrix[max_value_row_index, head_col_index] < a_matrix[next_row_index, head_col_index]:
-            max_value_row_index = next_row_index
-
-    if max_value_row_index>head_row_index :
-        print(f"head_row_index={head_row_index} and max_value_row_index={max_value_row_index}")
-        ## need to create a new array instead of refering the index.  Refering the index is like pass by reference and
-        ## that would not swap
-        temp                            =   np.array(a_matrix[max_value_row_index])
-        a_matrix[max_value_row_index]   =   a_matrix[head_row_index]
-        a_matrix[head_row_index]        =   temp
-
-    if a_matrix[max_value_row_index, head_col_index] == 0:
-        raise  ERROR_IN_PARTITIAL_PIVOT_STEPS(f" Multiple solutions exist as augmented matrix = {a_matrix} "
-                                              "row {max_value_rc_index}  and {curr_row_index} is zero")
-    return a_matrix
-
-def elimination(a_matrix):
-    """
-    :param a_matrix:
-    :param n:
-    :return:
-    """
-    ## for each row until n-1 rows.  This loop is n-1 rows because this is the row compared with the next row
-    ## hence next row counter is till n.  However, in python range is not inclusive and hence need to give till n
+    def __init__(self,precision=5):
+        self.precision = precision
 
 
-    for i in range(0, len(a_matrix) - 1, 1):
-        cc = i
-        a_matrix = partial_pivot(a_matrix,i)
-        for nr in range (i+1, len(a_matrix), 1):
-            coefficient = a_matrix[nr][cc] / a_matrix[i][cc]
-            for cc1 in range(cc, len(a_matrix[0]), 1):
-                a_matrix[nr, cc1] = a_matrix[nr, cc1] - (coefficient * a_matrix[i, cc1])
+    def partial_pivot(self,a_matrix:np.array, head_row_index:int)->np.array:
+        """
+        partial_pivot methods applies row pivot on a_matrix by comparing curr_row_index with subsequent rows
+        and swaps the rows if the a_matrix[curr_row_index,column_index] with a_matrix[max_row_index,column_index]
+        :param a_matrix:
+        :param head_row_index:
+        :return:
+        """
+        max_value_row_index = head_row_index
+        ## This is same as row_index because, the guass elimination works with nxn matrix
+        head_col_index     = head_row_index
+       # print(f"head_row_index={head_row_index} and length = {len(a_matrix)} range = {range(head_row_index + 1, 1, len(a_matrix)+1)}")
+        for next_row_index in range(head_row_index + 1, len(a_matrix),1):
+        #    print(f" printing next_row_index ={next_row_index} ")
+            if a_matrix[max_value_row_index, head_col_index] < a_matrix[next_row_index, head_col_index]:
+                max_value_row_index = next_row_index
+
+        if max_value_row_index>head_row_index :
+            ## print(f"head_row_index={head_row_index} and max_value_row_index={max_value_row_index}")
+            ## need to create a new array instead of refering the index.  Refering the index is like pass by reference and
+            ## that would not swap
+            temp                            =   np.array(a_matrix[max_value_row_index])
+            a_matrix[max_value_row_index]   =   a_matrix[head_row_index]
+            a_matrix[head_row_index]        =   temp
+
+        if a_matrix[max_value_row_index, head_col_index] == 0:
+            raise  ERROR_IN_PARTITIAL_PIVOT_STEPS(f" Multiple solutions exist as augmented matrix = {a_matrix} "
+                                                  "row {max_value_rc_index}  and {curr_row_index} is zero")
+        return a_matrix
+
+    def elimination(self,a_matrix):
+        """
+        :param a_matrix:
+        :param n:
+        :return:
+        """
+        ## for each row until n-1 rows.  This loop is n-1 rows because this is the row compared with the next row
+        ## hence next row counter is till n.  However, in python range is not inclusive and hence need to give till n
 
 
-    if a_matrix[len(a_matrix) - 1, len(a_matrix) - 1] == 0:
-        raise ERROR_IN_ELEMENTATION_STEPS(f" Matrix nxn cell is 0 and hence solution is not possible.  "
-                                          f" Matrix = {a_matrix}"
-                                          f" aug_matrix[{len(a_matrix) - 1},{len(a_matrix) - 1}] = "
-                                          f" {a_matrix[len(a_matrix) - 1, len(a_matrix) - 1]}")
+        for i in range(0, len(a_matrix) - 1, 1):
+            cc = i
+            a_matrix = self.partial_pivot(a_matrix,i)
+            for nr in range (i+1, len(a_matrix), 1):
+                coefficient = round(a_matrix[nr][cc] / a_matrix[i][cc],self.precision)
+                for cc1 in range(cc, len(a_matrix[0]), 1):
+                    a_matrix[nr, cc1] = round(a_matrix[nr, cc1] - (coefficient * a_matrix[i, cc1]),self.precision)
 
 
-    return a_matrix
+        if a_matrix[len(a_matrix) - 1, len(a_matrix) - 1] == 0:
+            raise ERROR_IN_ELEMENTATION_STEPS(f" Matrix nxn cell is 0 and hence solution is not possible.  "
+                                              f" Matrix = {a_matrix}"
+                                              f" aug_matrix[{len(a_matrix) - 1},{len(a_matrix) - 1}] = "
+                                              f" {a_matrix[len(a_matrix) - 1, len(a_matrix) - 1]}")
 
 
-def back_substituion(a_matrix):
-    """
-    back substitution finds the last row's x(n) value and uses the substitues that value in the previous equation to find
-    previous rows x value i,e x(n-1)
-    :param a_matrix:  matrix after bring the matrix into upper triangle matrix, this is augmented matrix
-    :param n: this is aug_matrix rows -1 because array index starts from 0
-    :return:
-    """
-    n       = len(a_matrix)
-    x       =   np.zeros(n, dtype='float32')
-    x[n-1]    = a_matrix[n-1, n] / a_matrix[n-1, n-1]
-    ## remember x[n-1] already filled so start from n-2
+        return a_matrix
 
 
-    for i in range(n-2, -1,-1):
-        coproduct = 0
-        for j in range (i+1, n):
-            coproduct = coproduct + a_matrix[i, j] * x[j]
-            print(f"coproduct={coproduct} i={i} j={j} x[j={j}] = {x[j]} and a_matrix[i={i}, j={j}] = {a_matrix[i, j]}")
-        x[i] = (1 / a_matrix[i, i]) * (a_matrix[i, n] - coproduct)
-    return x
+    def back_substitution(self,a_matrix):
+        """
+        back substitution finds the last row's x(n) value and uses the substitues that value in the previous equation to find
+        previous rows x value i,e x(n-1)
+        :param a_matrix:  matrix after bring the matrix into upper triangle matrix, this is augmented matrix
+        :param n: this is aug_matrix rows -1 because array index starts from 0
+        :return:
+        """
+        n       = len(a_matrix)
+        x       = np.zeros(n, dtype='float32')
+        x[n-1]  = round(a_matrix[n-1, n] / a_matrix[n-1, n-1],self.precision)
+        ## remember x[n-1] already filled so start from n-2
+
+
+        for i in range(n-2, -1,-1):
+            coproduct = 0
+            for j in range (i+1, n):
+                coproduct = round(coproduct + a_matrix[i, j] * x[j],self.precision)
+                print(f"coproduct={coproduct} i={i} j={j} x[j={j}] = {x[j]} and a_matrix[i={i}, j={j}] = {a_matrix[i, j]}")
+            x[i] = round((1 / a_matrix[i, i]) * (a_matrix[i, n] - coproduct),self.precision)
+        return x
 
 
 
-def guass_elimination(aug_matrix:np.array)->np.array:
-    """
-    guass_elimination first implements eliminates the row elements to get the upper triangle format
-    it then applies back substitution and solves the equation.  It is expected that the input matrix
-    is nxn matrix and augumented matrix is nxn+1 matrix
-    :param aug_matrix:  Augmented matrix is Matrix + R.H.S column vector.
-    :param aug_mat_rows: This is same as input matrix number of rows
-    :param aug_mat_cols: This is augmented i.,e  input may have n columns then add RHS vector and it becomes n+1 columns
-    """
-    aug_mat_rows = len(aug_matrix)
-    aug_mat_cols = len(aug_matrix[0])
-    if (aug_mat_cols - 1 != aug_mat_rows):
-        raise " Not a valid matrix.  Matrix must be in nxn and augumented matrix must be nxn+1"
-    # declare few variables to process the algorthm
+    def guass_elimination(self,aug_matrix:np.array)->np.array:
+        """
+        guass_elimination first implements eliminates the row elements to get the upper triangle format
+        it then applies back substitution and solves the equation.  It is expected that the input matrix
+        is nxn matrix and augumented matrix is nxn+1 matrix
+        :param aug_matrix:  Augmented matrix is Matrix + R.H.S column vector.
+        :param aug_mat_rows: This is same as input matrix number of rows
+        :param aug_mat_cols: This is augmented i.,e  input may have n columns then add RHS vector and it becomes n+1 columns
+        """
+        aug_mat_rows = len(aug_matrix)
+        aug_mat_cols = len(aug_matrix[0])
+        if (aug_mat_cols - 1 != aug_mat_rows):
+            raise " Not a valid matrix.  Matrix must be in nxn and augumented matrix must be nxn+1"
+        # declare few variables to process the algorthm
 
-    n = aug_mat_rows-1 ## as matrix index starts from 0
-    try:
-        aug_matrix      = elimination(aug_matrix)
-        output_x_vector = back_substituion(aug_matrix)
-        return output_x_vector
-    except Exception as e:
-        print(f"Exception {e}")
+        try:
+            aug_matrix      = self.elimination(aug_matrix)
+            output_x_vector = self.back_substitution(aug_matrix)
+            return output_x_vector
+        except Exception as e:
+            print(f"Exception {e}")
 
-    return None
+        return None
+
+
--- question-2/test_gauss_elimination2.py
+++ question-2/test_gauss_elimination2.py
@@ -4,50 +4,62 @@ from gauss_elimination2 import *
 
 class Test(TestCase):
 
+
+
     def test_partitial_pivot(self):
         ran = range(0,10,-1)
         for i in ran:
             print(i)
+
         a_matrix = np.array([[1.0, 2.0, 7.0], [3.0, 4.0, 8.0]],dtype=float)
-        a_matrix = partial_pivot(a_matrix, 0)
+        gaussObject = GaussElimination(3)
+        a_matrix = gaussObject.partial_pivot(a_matrix, 0)
         print(a_matrix)
 
     def test_elimination(self):
         aug_matrix  =   np.array([[3,4,8],[1,2,7]],dtype=float)
-        aug_matrix  = elimination(aug_matrix)
+        gaussObject = GaussElimination(2)
+        aug_matrix  = gaussObject.elimination(aug_matrix)
         print(aug_matrix)
 
     def test_elimination_many_solution(self):
         aug_matrix  =   np.array([[1,2,7],[1,2,8]],dtype=float)
-        aug_matrix  = elimination(aug_matrix)
+        gaussObject = GaussElimination()
+        aug_matrix  = gaussObject.elimination(aug_matrix)
         print(aug_matrix)
 
     def test_back_substituion(self):
         aug_matrix  =   np.array([[1,2,7],[0,0.667,4.333]],dtype=float)
-        x           =   back_substituion(aug_matrix)
+        gaussObject = GaussElimination(2)
+        x           =   gaussObject.back_substitution(aug_matrix)
         print(x)
 
     def test_back_substituion_2(self):
         aug_matrix  =   np.array([[3,4,8,9],[0,1,3,7],[0,0,1,2]],dtype=float)
-        x           =   back_substituion(aug_matrix)
+        gaussObject = GaussElimination(4)
+        x           =   gaussObject.back_substitution(aug_matrix)
         print(x)
 
     def test_guass_elimination(self):
         aug_matrix  =   np.array([[1,2,7],[3,4,8]],dtype=float)
-        x           =   guass_elimination(aug_matrix)
+        gaussObject = GaussElimination(4)
+        x           =  gaussObject.guass_elimination(aug_matrix)
         print(x)
 
     def test_guass_elimination_error_test(self):
         aug_matrix  =   np.array([[1,2,7],[1,2,8]],dtype=float)
-        x           =   guass_elimination(aug_matrix)
+        gaussObject = GaussElimination(4)
+        x           = gaussObject.guass_elimination(aug_matrix)
         print(x)
 
     def test_guass_elimination_3(self):
         aug_matrix  =   np.array([[1,2,-1,6],[3,8,9,10],[2,-1,2,-2]],dtype=float)
-        x           =   guass_elimination(aug_matrix)
+        gaussObject =   GaussElimination(2)
+        x           =   gaussObject.guass_elimination(aug_matrix)
         print(x)
 
     def test_guass_elimination_4(self):
         aug_matrix  =   np.array([[1,2,3,18],[2,1,-4,-30],[-5,8,17,96]],dtype=float)
-        x           =   guass_elimination(aug_matrix)
+        gaussObject =   GaussElimination()
+        x           =   gaussObject.guass_elimination(aug_matrix)
         print(x)
